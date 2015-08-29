8a76c3b28243c218ba7bc7f77c34afeb2b8981b7
unknown <tom.tang@H2600-0456L.perficient.com>
2015-08-29 16:00:35 +0800

    add

--- sites/all/modules/review360_base/template/member/review360_interviewer_exam_choose.tpl.php
+++ sites/all/modules/review360_base/template/member/review360_interviewer_exam_choose.tpl.php
@@ -1,5 +1,8 @@
 <?php 
     global $base_url;
+//    $user = get_user_infor_by_key($user_key);
+    $exam_nids = get_user_exam_by_key($user_key);
+    dd($exam_nids);
 ?>
 <div class="row">
      <div class="page-header">
@@ -22,10 +25,29 @@
             </div>
         </div>
     </div>
-    <div class="col-md-6 col-md-offset-3" >
-        <?php 
-        empty;
-        ?> 
-        
+    <div class="col-md-10 col-md-offset-1" >
+        <table class="table bs-example table-striped table-responsive table-hover" style="">
+      <!--<caption>用户分组列表：</caption>-->
+        <thead>
+            <tr>
+                <th>操作</th>
+                <th>评测</th>
+                <th>状态</th>
+                <th>预计时间</th>
+            </tr>
+        </thead>
+        <tbody id="exam_table_tb">
+            <tr>
+                <td class="" style="line-height:60px;">
+                    <a class="btn btn-primary " 
+                            id="edit-exam-name-btn" 
+                             type="submit">开始评测</a>
+                </td class="" style="line-height:60px;">
+                <td class=""  style="line-height:60px;border-bottom:1px solid #ddd">风向认证0.1</td>
+                <td class=""  style="line-height:60px;">24</td>
+                <td class=""  style="line-height:60px;">12分钟</td>
+            </tr>
+        </tbody>
+    </table> 
     </div>
 </div>
