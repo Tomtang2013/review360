4dab6fa0642ee366a4907abb01e42e15c6c4daa5
unknown <tom.tang@H2600-0456L.perficient.com>
2015-10-09 10:30:54 +0800

    remvoe footer change css

--- sites/all/themes/review360/template.php
+++ sites/all/themes/review360/template.php
@@ -2,5 +2,7 @@
 
 
 function review360_system_powered_by(){
-    return '<span>' . t('Powered by <a href="@poweredby">Tom Tang</a>', array('@poweredby' => '')) . '</span>';
+    
+    return '<div style="text-align:center;width:100%;"><span>' . t('<a href="@poweredby">关于我们</a>', array('@poweredby' => '')) . '</span>'
+        . " | ". '<span>' . t('<a href="@poweredby">联系方式</a>', array('@poweredby' => '')) . '</span></div>';
 }
