a4c66ecb285d33f0979f30fd57168f88836195c1
Tom <tom.tang@perficient.com>
2015-10-11 16:58:25 +0800

    add test method

--- sites/all/modules/review360_base/lib/ReportGenerator/ImageGenerator/ImageCreater.php
+++ sites/all/modules/review360_base/lib/ReportGenerator/ImageGenerator/ImageCreater.php
@@ -1,44 +1,87 @@
-<?php	
-	
-	class ImageCreater{
-		
-		private $_imageName;
-		private $_background;
-		private $_data;
-		
-		public function __construct($imageName,$background,$data) {
-			$this->_imageName = $imageName;
-			$this->_background = $background;
-			$this->_data = $data;
-		}			
-		
-		public function generate_image(){
-//			$image = imagecreatefrompng($this->_background);
-			$image = imagecreatefromjpeg($this->_background);
-	
-	//		$gray_color = imagecolorallocate($image, 162, 162, 162);               
-	//		$red_color = imagecolorallocate($image, 255, 0, 0);  
-	//		$yellow_color = imagecolorallocate($image, 0, 255, 0);
-			$white_color = imagecolorallocate($image, 255, 255, 255);
-			imagefilledellipse($image, 50, 50, 20, 20, $white_color);  
-			imagefilledellipse($image, 120, 60, 20, 20, $white_color);  
-			imagefilledellipse($image, 195, 100, 20, 20, $white_color);  
-			imagefilledellipse($image, 265, 120, 20, 20, $white_color); 
-			 
-			imagesetthickness (  $image , 8 );
-			imageline($image, 50, 50, 120, 60, $white_color);
-			imageline($image, 120, 60, 195, 100, $white_color);
-			imageline($image, 195, 100, 265, 120, $white_color);
-			ob_clean();
-			flush();
-			
-			Imagejpeg($image,$this->_imageName);
-			ImageDestroy($image);
-		}
-		
-	}
+<?php
 
-	
-	
+class ImageCreater {
+
+    private $r = 15;
+    private $line_width = 5;
+    private $bar_width = 15;
+    private $d_x_offset = 50;
+    private $i_x_offset = 120;
+    private $s_x_offset = 195;
+    private $c_x_offset = 265;
+    private $top_100_y_offset = 5;
+    private $bottom_0_y_offset = 350;
+    private $middle_50_y_offset = 180;
+    private $_imageName;
+    private $_background;
+    private $_data;
+
+    public function __construct($imageName, $background, $data) {
+        $this->_imageName = $imageName;
+        $this->_background = $background;
+        $this->_data = $data;
+    }
+
+    private function calculate_value($value) {
+        $rate = ($this->bottom_0_y_offset - $this->top_100_y_offset) / 100;
+        return floor((100 - $value) * $rate);
+    }
+
+    public function generate_image($values, $is_line) {
+        if ($is_line) {
+            $this->render_image_line($values);
+        } else {
+            $this->render_image_bar($values);
+        }
+    }
+
+    private function render_image_bar($values) {
+        $image = imagecreatefromjpeg($this->_background);
+
+        $d = $this->calculate_value($values->d_value + 50);
+        $i = $this->calculate_value($values->i_value + 50);
+        $s = $this->calculate_value($values->s_value + 50);
+        $c = $this->calculate_value($values->c_value + 50);
+
+        $white_color = imagecolorallocate($image, 255, 255, 255);
+        imagefilledrectangle($image, $this->d_x_offset - $this->bar_width, $this->middle_50_y_offset, $this->d_x_offset + $this->bar_width, $d, $white_color);
+        imagefilledrectangle($image, $this->i_x_offset - $this->bar_width, $this->middle_50_y_offset, $this->i_x_offset + $this->bar_width, $i, $white_color);
+        imagefilledrectangle($image, $this->s_x_offset - $this->bar_width, $this->middle_50_y_offset, $this->s_x_offset + $this->bar_width, $s, $white_color);
+        imagefilledrectangle($image, $this->c_x_offset - $this->bar_width, $this->middle_50_y_offset, $this->c_x_offset + $this->bar_width, $c, $white_color);
+
+        ob_clean();
+        flush();
+
+        Imagejpeg($image, $this->_imageName);
+        ImageDestroy($image);
+    }
+
+    private function render_image_line($values) {
+//        $image = imagecreatefrompng($this->_background);
+        $image = imagecreatefromjpeg($this->_background);
+
+        $d = $this->calculate_value($values->d_value);
+        $i = $this->calculate_value($values->i_value);
+        $s = $this->calculate_value($values->s_value);
+        $c = $this->calculate_value($values->c_value);
+
+        $white_color = imagecolorallocate($image, 255, 255, 255);
+        imagefilledellipse($image, $this->d_x_offset, $d, $this->r, $this->r, $white_color);
+        imagefilledellipse($image, $this->i_x_offset, $i, $this->r, $this->r, $white_color);
+        imagefilledellipse($image, $this->s_x_offset, $s, $this->r, $this->r, $white_color);
+        imagefilledellipse($image, $this->c_x_offset, $c, $this->r, $this->r, $white_color);
+
+        imagesetthickness($image, $this->line_width);
+        imageline($image, $this->d_x_offset, $d, $this->i_x_offset, $i, $white_color);
+        imageline($image, $this->i_x_offset, $i, $this->s_x_offset, $s, $white_color);
+        imageline($image, $this->s_x_offset, $s, $this->c_x_offset, $c, $white_color);
+        ob_clean();
+        flush();
+
+        Imagejpeg($image, $this->_imageName);
+        ImageDestroy($image);
+    }
+
+}
 ?>
 
--- sites/all/modules/review360_base/lib/ReportGenerator/ReportCreator.php
+++ sites/all/modules/review360_base/lib/ReportGenerator/ReportCreator.php
@@ -14,15 +14,15 @@ class ReportCreator{
 
     }	
     public function generate_report($exam){
+
         require_once drupal_get_path('module', 'review360_base') . '/lib/PHPWord/PHPWord.php';
         require_once drupal_get_path('module', 'review360_base') . '/lib/ReportGenerator/ImageGenerator/ImageCreater.php';
         $PHPWord = new PHPWord();
-        //$document = $PHPWord->loadTemplate('TemplateCopy.docx');
         $document = $PHPWord->loadTemplate($this->_filePath);
-        $document->setValue('company_name', iconv('utf-8', 'gbk', '网易电子'));
-        $document->setValue('user_name', iconv('utf-8', 'gbk', '王启发'));
+        $document->setValue('company_name', iconv('utf-8', 'gbk', $exam->u_company_name));
+        $document->setValue('user_name', iconv('utf-8', 'gbk', $exam->u_name));
 
-        $document->setValue('position', iconv('utf-8', 'gbk', '高级警督'));
+        $document->setValue('position', iconv('utf-8', 'gbk', $exam->u_department));
         $document->setValue('date', iconv('utf-8', 'gbk', '2015年5月27日'));
         $document->setValue('work', iconv('utf-8', 'gbk', '动态 ==在工作情境中，你希望自己表现出的主要行为风格为D（支配型）。你希望自己可以表现出极高的成就感，好胜且有进取心，精力充沛，敢于挑战，果断自信，善于领导。'));
         $document->setValue('innate', iconv('utf-8', 'gbk', '动态 ==当你处于压力情境下时，你将会表现出自己最真实的动机与欲求，此时，你将会表现出的主要行为风格为D（支配型）、I（影响型）、S（稳健型）。此时，你目标明确，做事耐心坚定，精力充沛，善于交往。'));
@@ -34,16 +34,16 @@ class ReportCreator{
         $document->setValue('advantage', iconv('utf-8', 'gbk', '动态 ==直接的，果断的；你具有强大的自我力量，通常是问题的解决者；你是冒险家，动力通常来自于你自身。'));
         $document->setValue('deficiency', iconv('utf-8', 'gbk', '动态 ==你有强烈的支配行为特征，有时候却可能显得过于武断权威；好争论，或过于盛气凌人；不喜欢常规，喜欢冒险却可能行动过急。'));
         $document->setValue('work_manifestation', iconv('utf-8', 'gbk', '动态 ==你具有领导与控制的才干，这些都是基于你直接的、高标准要求的天性。比较起需要紧密关系结合的工作情境来说，更适应于结构化的、正式的工作情境。你是非常具有竞争力与自信的人群，在信息缺乏的情势下，仍然能够迅速做出决策并实施计划。在他人看来极度困难的任务，对于你来说却可能“如鱼得水”。'));
-
+       
         $img1 = new ImageCreater(DEFAULT_IMGPATH.'img1.png',DEFAULT_BGIMG,null);
         $img2 = new ImageCreater(DEFAULT_IMGPATH.'img2.png',DEFAULT_BGIMG,null);
         $img3 = new ImageCreater(DEFAULT_IMGPATH.'img3.png',DEFAULT_BGIMG,null);
         $img4 = new ImageCreater(DEFAULT_IMGPATH.'img4.png',DEFAULT_BGIMG,null);
 
-        $img1->generate_image();
-        $img3->generate_image();
-        $img2->generate_image();
-        $img4->generate_image();
+        $img1->generate_image($exam->rst->in_working_me_value,true);
+        $img2->generate_image($exam->rst->innate_value,true);
+        $img3->generate_image($exam->rst->other_value,true);
+        $img4->generate_image($exam->rst->stress_value,false);
 
         $document->save_image('image5.jpeg',DEFAULT_IMGPATH.'img1.png',$document);
         $document->save_image('image6.jpeg',DEFAULT_IMGPATH.'img2.png',$document);
