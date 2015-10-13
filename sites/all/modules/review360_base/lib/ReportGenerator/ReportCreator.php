d634832e0397bb7d92e072d457c784ad80eceae7
unknown <tom.tang@H2600-0456L.perficient.com>
2015-10-13 13:26:40 +0800

    add try catch

--- sites/all/modules/review360_base/lib/ReportGenerator/ReportCreator.php
+++ sites/all/modules/review360_base/lib/ReportGenerator/ReportCreator.php
@@ -14,67 +14,70 @@ class ReportCreator{
 
     }	
     public function generate_report($exam){
+        try{
+            require_once drupal_get_path('module', 'review360_base') . '/lib/PHPWord/PHPWord.php';
+            require_once drupal_get_path('module', 'review360_base') . '/lib/ReportGenerator/ImageGenerator/ImageCreater.php';
+            $PHPWord = new PHPWord();
+            $document = $PHPWord->loadTemplate($this->_filePath);
 
-        require_once drupal_get_path('module', 'review360_base') . '/lib/PHPWord/PHPWord.php';
-        require_once drupal_get_path('module', 'review360_base') . '/lib/ReportGenerator/ImageGenerator/ImageCreater.php';
-        $PHPWord = new PHPWord();
-        $document = $PHPWord->loadTemplate($this->_filePath);
+    //        $document->setValue('company_name', iconv('utf-8', 'gbk', $exam->u_company_name));
+            $document->setValue('name', iconv('utf-8', 'gbk', $exam->u_name));
 
-//        $document->setValue('company_name', iconv('utf-8', 'gbk', $exam->u_company_name));
-        $document->setValue('name', iconv('utf-8', 'gbk', $exam->u_name));
+    //        $document->setValue('position', iconv('utf-8', 'gbk', $exam->u_department));
+            $document->setValue('date', iconv('utf-8', 'gbk', date('Y年m月d日',$exam->update_date)));
 
-//        $document->setValue('position', iconv('utf-8', 'gbk', $exam->u_department));
-        $document->setValue('date', iconv('utf-8', 'gbk', date('Y年m月d日',$exam->update_date)));
-        
-//        $document->setValue('work', iconv('utf-8', 'gbk', '动态 ==在工作情境中，你希望自己表现出的主要行为风格为D（支配型）。你希望自己可以表现出极高的成就感，好胜且有进取心，精力充沛，敢于挑战，果断自信，善于领导。'));
-//        $document->setValue('innate', iconv('utf-8', 'gbk', '动态 ==当你处于压力情境下时，你将会表现出自己最真实的动机与欲求，此时，你将会表现出的主要行为风格为D（支配型）、I（影响型）、S（稳健型）。此时，你目标明确，做事耐心坚定，精力充沛，善于交往。'));
-//        $document->setValue('outside', iconv('utf-8', 'gbk', '动态 ==你在平时工作中，最常表现出来的行为风格为D（支配型）。你有着极高的成就感，好胜且有进取心，精力充沛，敢于挑战，果断自信，善于领导。'));
-        $document->setValue('suitable', iconv('utf-8', 'gbk', $exam->str->stress_desc->desc));
-       
-        $document->setValue('code', iconv('utf-8', 'gbk', $exam->str->rst_name));
-        $document->setValue('classic', iconv('utf-8', 'gbk', $exam->str->rst_characteristic));
-        $document->setValue('advantage', iconv('utf-8', 'gbk',$exam->str->rst_advantage));
-        $document->setValue('deficiency', iconv('utf-8', 'gbk', $exam->str->rst_insufficient));
-        $document->setValue('manifestation', iconv('utf-8', 'gbk', $exam->str->rst_work_performance));
+    //        $document->setValue('work', iconv('utf-8', 'gbk', '动态 ==在工作情境中，你希望自己表现出的主要行为风格为D（支配型）。你希望自己可以表现出极高的成就感，好胜且有进取心，精力充沛，敢于挑战，果断自信，善于领导。'));
+    //        $document->setValue('innate', iconv('utf-8', 'gbk', '动态 ==当你处于压力情境下时，你将会表现出自己最真实的动机与欲求，此时，你将会表现出的主要行为风格为D（支配型）、I（影响型）、S（稳健型）。此时，你目标明确，做事耐心坚定，精力充沛，善于交往。'));
+    //        $document->setValue('outside', iconv('utf-8', 'gbk', '动态 ==你在平时工作中，最常表现出来的行为风格为D（支配型）。你有着极高的成就感，好胜且有进取心，精力充沛，敢于挑战，果断自信，善于领导。'));
+            $document->setValue('suitable', iconv('utf-8', 'gbk', $exam->str->stress_desc->desc));
 
-        $img1 = new ImageCreater(DEFAULT_IMGPATH.'img1.png',DEFAULT_BGIMG,null);
-        $img2 = new ImageCreater(DEFAULT_IMGPATH.'img2.png',DEFAULT_BGIMG,null);
-        $img3 = new ImageCreater(DEFAULT_IMGPATH.'img3.png',DEFAULT_BGIMG,null);
-        $img4 = new ImageCreater(DEFAULT_IMGPATH.'img4.png',DEFAULT_BGIMG,null);
+            $document->setValue('code', iconv('utf-8', 'gbk', $exam->str->rst_name));
+            $document->setValue('classic', iconv('utf-8', 'gbk', $exam->str->rst_characteristic));
+            $document->setValue('advantage', iconv('utf-8', 'gbk',$exam->str->rst_advantage));
+            $document->setValue('deficiency', iconv('utf-8', 'gbk', $exam->str->rst_insufficient));
+            $document->setValue('manifestation', iconv('utf-8', 'gbk', $exam->str->rst_work_performance));
 
-        $img1->generate_image($exam->rst->in_working_me_value,true);
-        $img2->generate_image($exam->rst->innate_value,true);
-        $img3->generate_image($exam->rst->other_value,true);
-        $img4->generate_image($exam->rst->stress_value,false);
+            $img1 = new ImageCreater(DEFAULT_IMGPATH.'img1.png',DEFAULT_BGIMG,null);
+            $img2 = new ImageCreater(DEFAULT_IMGPATH.'img2.png',DEFAULT_BGIMG,null);
+            $img3 = new ImageCreater(DEFAULT_IMGPATH.'img3.png',DEFAULT_BGIMG,null);
+            $img4 = new ImageCreater(DEFAULT_IMGPATH.'img4.png',DEFAULT_BGIMG,null);
 
-        $document->save_image('image4.jpeg',DEFAULT_IMGPATH.'img1.png',$document);
-        $document->save_image('image5.jpeg',DEFAULT_IMGPATH.'img2.png',$document);
-        $document->save_image('image6.jpeg',DEFAULT_IMGPATH.'img3.png',$document);
-        $document->save_image('image7.jpeg',DEFAULT_IMGPATH.'img4.png',$document);
-//        $document->save_image('image11.jpeg',DEFAULT_IMGPATH.'img4.png',$document);
+            $img1->generate_image($exam->rst->in_working_me_value,true);
+            $img2->generate_image($exam->rst->innate_value,true);
+            $img3->generate_image($exam->rst->other_value,true);
+            $img4->generate_image($exam->rst->stress_value,false);
 
-       
-        if($exam->str->high_light == 'd'){
-            $doc = str_replace('w:val="A1A1A1"', 'w:val="000000"', $document->documentXML()); //D
-        } else if($exam->str->high_light == 'i'){
-            $doc = str_replace('w:val="A2A2A2"', 'w:val="000000"', $document->documentXML()); //i
-        } else if($exam->str->high_light == 'c'){
-            $doc = str_replace('w:val="A3A3A3"', 'w:val="000000"', $document->documentXML()); //s
-        } else if($exam->str->high_light == 's'){
-            $doc = str_replace('w:val="A4A4A4"', 'w:val="000000"', $document->documentXML()); //c
+            $document->save_image('image4.jpeg',DEFAULT_IMGPATH.'img1.png',$document);
+            $document->save_image('image5.jpeg',DEFAULT_IMGPATH.'img2.png',$document);
+            $document->save_image('image6.jpeg',DEFAULT_IMGPATH.'img3.png',$document);
+            $document->save_image('image7.jpeg',DEFAULT_IMGPATH.'img4.png',$document);
+    //        $document->save_image('image11.jpeg',DEFAULT_IMGPATH.'img4.png',$document);
+
+
+            if($exam->str->high_light == 'd'){
+                $doc = str_replace('w:val="A1A1A1"', 'w:val="000000"', $document->documentXML()); //D
+            } else if($exam->str->high_light == 'i'){
+                $doc = str_replace('w:val="A2A2A2"', 'w:val="000000"', $document->documentXML()); //i
+            } else if($exam->str->high_light == 'c'){
+                $doc = str_replace('w:val="A3A3A3"', 'w:val="000000"', $document->documentXML()); //s
+            } else if($exam->str->high_light == 's'){
+                $doc = str_replace('w:val="A4A4A4"', 'w:val="000000"', $document->documentXML()); //c
+            }
+    //        $doc = str_replace('w:val="A1A1A1"', 'w:val="000000"', $document->documentXML()); //D
+    //        $doc = str_replace('w:val="A2A2A2"', 'w:val="ee82ee"', $doc);//I
+    //        $doc = str_replace('w:val="A3A3A3"', 'w:val="98fb98"', $doc);//c
+    //        $doc = str_replace('w:val="A4A4A4"', 'w:val="a0522d"', $doc);//s
+            $doc = str_replace('w:themeColor="background2"', '', $doc);
+
+            $document->set_documentXML($doc);
+            $pix = $exam->nick_name."_".$exam->u_name;
+            $file_name = $this->_outPutPath."/report_".$pix.".doc";
+            $file_name = iconv('utf-8','gbk',$file_name); 
+            $document->save($file_name);
+            return $file_name;
+        } catch (Exception $ex) {
+            dd($exam->u_name);
         }
-//        $doc = str_replace('w:val="A1A1A1"', 'w:val="000000"', $document->documentXML()); //D
-//        $doc = str_replace('w:val="A2A2A2"', 'w:val="ee82ee"', $doc);//I
-//        $doc = str_replace('w:val="A3A3A3"', 'w:val="98fb98"', $doc);//c
-//        $doc = str_replace('w:val="A4A4A4"', 'w:val="a0522d"', $doc);//s
-        $doc = str_replace('w:themeColor="background2"', '', $doc);
-
-        $document->set_documentXML($doc);
-        $pix = $exam->nick_name."_".$exam->u_name;
-        $file_name = $this->_outPutPath."/report_".$pix.".doc";
-        $file_name = iconv('utf-8','gbk',$file_name); 
-        $document->save($file_name);
-        return $file_name;
     }
 }
 ?>
